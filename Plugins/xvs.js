16ef892c274526fd539dc300d776ed9d53f4b819
alex piku <122716205+Piku090909@users.noreply.github.com>
2024-05-14 08:30:15 +0530

    Update xvs.js

--- Plugins/xvs.js
+++ Plugins/xvs.js
@@ -19,15 +19,15 @@ var data = await fek.json();
 > *DURATION* : ${data.result[i].duration}
 > *URL* : ${data.result[i].url}\n`
         }
-        txt+=`\n〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»`
+        txt+=`\nÀ𝖑è𝖝 𝕻ì𝖐ù ː͢»`
                    await message.client.sendMessage(message.jid,{ document :{ url: "https://www.mediafire.com/file/n1qjfxjgvt0ovm2/IMG-20240211-WA0086_%25281%2529.pdf/file" }, fileName: "𝗫 𝗩𝗜𝗗𝗘𝗢 𝗥𝗘𝗦𝗨𝗟𝗧𝗦" , mimetype: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileLength: "999999950", contextInfo: { externalAdReply: {
-title: "〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»",
+title: "À𝖑è𝖝 𝕻ì𝖐ù ː͢»",
 body: "",
 sourceUrl: "",
 mediaUrl: "",
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: false,
-thumbnailUrl: "https://i.imgur.com/Q49D1NN.mp4" }}, caption: (txt)}, {quoted: message })
+thumbnailUrl: "https://telegra.ph/file/03fd14a9266797b4227cb.jpg" }}, caption: (txt)}, {quoted: message })
     }
     );
